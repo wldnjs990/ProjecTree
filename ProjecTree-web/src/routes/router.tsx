be8e5f3fd9a43a5855a2812284736f87f84bf79c
jiwon <wl990@naver.com>
2026-01-19 11:29:24 +0900

    chore : 프론트엔드 프로젝트 환경설정

---
+++ ProjecTree-web/src/routes/router.tsx
@@ -0,0 +1,8 @@
+import NotFoundPage from '@/pages/NotFoundPage';
+import { createBrowserRouter } from 'react-router';
+import { publicRoutes } from '@/routes/publicRoutes';
+
+export const router = createBrowserRouter([
+  ...publicRoutes,
+  { path: '*', element: <NotFoundPage /> },
+]);
