7e4b2c689b7013ca7e8096090def14ff29cfa143
성민제 <minj21@naver.com>
2026-01-19 13:32:57 +0900

    feat: 공통응답 실패코드 5자리로 변경

--- ProjecTree-was/src/main/java/com/ssafy/projectree/global/api/code/ErrorCode.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/global/api/code/ErrorCode.java
@@ -3,19 +3,22 @@ package com.ssafy.projectree.global.api.code;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_FOUND_ERROR(DomainCode.USER, ExceptionCode.NOT_FOUND, "USER_NOT_FOUND_ERROR"),
+    USER_NOT_FOUND_ERROR(HttpStatus.NOT_FOUND, DomainCode.USER, ExceptionCode.NOT_FOUND, "USER_NOT_FOUND_ERROR"),
     ;
 
+    private HttpStatus httpStatus;
     private DomainCode domainCode;
     private ExceptionCode exceptionCode;
     @Getter
     private String defaultMessage;
 
+    //4040101
     public int status() {
-        return domainCode.getValue() * 100 + exceptionCode.getValue();
+        return httpStatus.value() * 10000 + domainCode.getValue() * 100 + exceptionCode.getValue();
     }
 
 
