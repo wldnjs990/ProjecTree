19ffb9031f635901dc10d5e98128c76451ef1312
이성훈 <qkghwkd153@naver.com>
2026-01-27 00:51:29 +0900

    feat: node positions 저장 api 구현

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
@@ -1,5 +1,6 @@
 package com.ssafy.projectree.domain.node.api.controller;
 
+import com.ssafy.projectree.domain.node.api.dto.NodePositionUpdateDto;
 import com.ssafy.projectree.domain.node.api.dto.NodeUpdateDto;
 import com.ssafy.projectree.global.api.code.SuccessCode;
 import com.ssafy.projectree.global.api.response.CommonResponse;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +27,19 @@ public class NodeInternalController {
     ) {
 
         log.info("NodeDetail save request:{}", request);
+        return CommonResponse.success(SuccessCode.UPDATED, null);
+    }
+
+    @PutMapping("/nodes/positions")
+    public CommonResponse<Void> updateNodePositions(
+            @PathVariable String workspaceId,
+            @RequestBody NodePositionUpdateDto.Request request
+    ) {
+
+        log.info("NodePosition update request:{}", request);
+
+        // TODO: 배치처리 필요
+
         return CommonResponse.success(SuccessCode.UPDATED, null);
     }
 }
