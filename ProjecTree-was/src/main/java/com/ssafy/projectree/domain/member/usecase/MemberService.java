b57fcdeebcb0bbe7f6047e8768dd4f0b5332f821
진경석 <dev.jkjin112@gmail.com>
2026-01-27 15:18:45 +0900

    feat: 비즈니스 로직 처리를 위한 member 로직 추가

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/member/usecase/MemberService.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/member/usecase/MemberService.java
@@ -1,5 +1,7 @@
 package com.ssafy.projectree.domain.member.usecase;
 
+import com.ssafy.projectree.domain.member.model.entity.Member;
+import com.ssafy.projectree.domain.member.model.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    // TODO: 예외 발생에 대한 처리 필요
+    public Member findById(Long id) {
+        return memberRepository.findById(id).get();
+    }
+
+    // TODO: 예외 발생에 대한 처리 필요
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email).get();
+    }
+
 }
