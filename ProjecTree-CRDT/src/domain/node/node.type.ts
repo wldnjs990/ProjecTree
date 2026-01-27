d473ffb94efe51e04881990f6f8655f65c869f0f
LeeSH <dlfyd05680@gmail.com>
2026-01-27 16:14:28 +0900

    refactor: 노드 타입 ADVANCE 추가

--- ProjecTree-CRDT/src/domain/node/node.type.ts
+++ ProjecTree-CRDT/src/domain/node/node.type.ts
@@ -1,4 +1,4 @@
-export type NodeType = "PROJECT" | "EPIC" | "STORY" | "TASK";
+export type NodeType = "PROJECT" | "EPIC" | "STORY" | "TASK" | "ADVANCE";
 export type TaskType = "FE" | "BE" | null;
 export type NodeStatus = "TODO" | "IN_PROGRESS" | "DONE";
 export type Priority = "P0" | "P1" | "P2" | "P3";
