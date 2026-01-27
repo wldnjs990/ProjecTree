96031a59e4ca909f7198ffbc1d673c08e1d8a986
이성훈 <qkghwkd153@naver.com>
2026-01-28 00:12:26 +0900

    feat: 노드 위치 비동기 업데이트 기능 추가

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
@@ -2,6 +2,7 @@ package com.ssafy.projectree.domain.node.api.controller;
 
 import com.ssafy.projectree.domain.node.api.dto.NodePositionUpdateDto;
 import com.ssafy.projectree.domain.node.api.dto.NodeUpdateDto;
+import com.ssafy.projectree.domain.node.usecase.NodeAsyncService;
 import com.ssafy.projectree.global.api.code.SuccessCode;
 import com.ssafy.projectree.global.api.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class NodeInternalController {
 
+    private final NodeAsyncService nodeAsyncService;
+
     @PostMapping("/nodes/{nodeId}/detail")
     public CommonResponse<Void> saveNodeDetail(
             @PathVariable Long workspaceId,
@@ -26,7 +29,6 @@ public class NodeInternalController {
             @RequestBody NodeUpdateDto.Request request
     ) {
 
-        log.info("NodeDetail save request:{}", request);
         return CommonResponse.success(SuccessCode.UPDATED, null);
     }
 
@@ -35,11 +37,12 @@ public class NodeInternalController {
             @PathVariable Long workspaceId,
             @RequestBody NodePositionUpdateDto.Request request
     ) {
+        nodeAsyncService.savePositionAsync(
+                workspaceId,
+                request.getNodes()
+        );
 
-        log.info("NodePosition update request:{}", request);
-
-        // TODO: 배치처리 필요
-
-        return CommonResponse.success(SuccessCode.UPDATED, null);
+        // 접수 완료
+        return CommonResponse.success(SuccessCode.UPDATED, null); // 202
     }
 }
