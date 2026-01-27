c1fde9a1ba12a56f0fb2a4a5ea0e45b0859fd099
이성훈 <qkghwkd153@naver.com>
2026-01-28 01:17:09 +0900

    fix: node positions API의 메서드를 Put 에서 Patch로 변경

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
@@ -7,9 +7,9 @@ import com.ssafy.projectree.global.api.code.SuccessCode;
 import com.ssafy.projectree.global.api.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +32,7 @@ public class NodeInternalController {
         return CommonResponse.success(SuccessCode.UPDATED, null);
     }
 
-    @PutMapping("/nodes/positions")
+    @PatchMapping("/nodes/positions")
     public CommonResponse<Void> updateNodePositions(
             @PathVariable Long workspaceId,
             @RequestBody NodePositionUpdateDto.Request request
