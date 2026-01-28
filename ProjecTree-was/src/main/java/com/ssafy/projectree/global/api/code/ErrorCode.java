b918755e0fbca54d2e2f25b8cd785ecff1627c0c
진경석 <dev.jkjin112@gmail.com>
2026-01-28 23:19:19 +0900

    feat: workspace not found 예외처리 추가

--- ProjecTree-was/src/main/java/com/ssafy/projectree/global/api/code/ErrorCode.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/global/api/code/ErrorCode.java
@@ -22,7 +22,8 @@ public enum ErrorCode {
     TOKEN_MALFORMED(DomainCode.AUTH, ExceptionCode.MALFORMED, "TOKEN_MALFORMED - 토큰 위조됨" ),
     TOKEN_INVALID_SIGNATURE(DomainCode.AUTH, ExceptionCode.INVALID,"TOKEN_INVALID_SIGNATURE - 서명 불일치" ),
     TOKEN_EMPTY(DomainCode.AUTH, ExceptionCode.EMPTY,"TOKEN_EMPTY-토큰 비어있음" ),
-    UN_EXPECTED_TOKEN_VALIDATION(DomainCode.AUTH, ExceptionCode.UN_EXPECTED ,"UN_EXPECTED - 예기치 못한 토큰" );
+    UN_EXPECTED_TOKEN_VALIDATION(DomainCode.AUTH, ExceptionCode.UN_EXPECTED ,"UN_EXPECTED - 예기치 못한 토큰" ),
+    WORKSPACE_NOT_FOUND(DomainCode.WORKSPACE, ExceptionCode.NOT_FOUND, "WORKSPACE_NOT_FOUND");
 
     private DomainCode domainCode;
     private ExceptionCode exceptionCode;
