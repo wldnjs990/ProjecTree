adf5de6b11ba7bdd80d097781ca2219c4da2f249
jiwon <wl990@naver.com>
2026-01-28 22:25:10 +0900

    feat : todo 리스트 생성

--- ProjecTree-web/src/apis/oauth.api.ts
+++ ProjecTree-web/src/apis/oauth.api.ts
@@ -11,4 +11,7 @@ const redirectToGithubOauth = () => {
   window.location.href = `${BASE_URL}oauth2/authorization/github?redirectURL=${redirectURL}`;
 };
 
+// TODO : oauth/callback으로 리다이렉트 될 시 서버로 refresh 쿠키 요청하는 API 전송하기
+const getRefreshToken = () => {};
+
 export { redirectToGoogleOauth, redirectToGithubOauth };
