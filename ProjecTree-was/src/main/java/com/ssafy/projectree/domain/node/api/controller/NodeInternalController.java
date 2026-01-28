b3067e2bb2ed42e8b29bc4d2d8c7f79d29e87a61
이성훈 <qkghwkd153@naver.com>
2026-01-28 13:04:40 +0900

    refactor: 동시편집 관련 메서드post->patch로 메서드 변경

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/controller/NodeInternalController.java
@@ -7,9 +7,8 @@ import com.ssafy.projectree.global.api.code.SuccessCode;
 import com.ssafy.projectree.global.api.response.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +21,7 @@ public class NodeInternalController {
 
     private final NodeAsyncService nodeAsyncService;
 
-    @PostMapping("/nodes/{nodeId}/detail")
+    @PatchMapping("/nodes/{nodeId}/detail")
     public CommonResponse<Void> saveNodeDetail(
             @PathVariable Long workspaceId,
             @PathVariable Long nodeId,
@@ -32,7 +31,7 @@ public class NodeInternalController {
         return CommonResponse.success(SuccessCode.UPDATED, null);
     }
 
-    @PutMapping("/nodes/positions")
+    @PatchMapping("/nodes/positions")
     public CommonResponse<Void> updateNodePositions(
             @PathVariable Long workspaceId,
             @RequestBody NodePositionUpdateDto.Request request
