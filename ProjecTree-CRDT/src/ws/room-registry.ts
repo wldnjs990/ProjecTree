a7f21a2c0b6d73052688938a057e053e88d28bfe
LeeSH <dlfyd05680@gmail.com>
2026-01-28 21:24:54 +0900

    fix: 사용하지 않는 yjs 코드 제거

--- ProjecTree-CRDT/src/ws/room-registry.ts
+++ ProjecTree-CRDT/src/ws/room-registry.ts
@@ -1,21 +1,16 @@
 import { WebSocket } from "ws";
-import * as Y from "yjs";
 
 export const rooms = new Map<string, Set<WebSocket>>();
-export const docs = new Map<string, Y.Doc>();
 
 export function getOrCreateRoom(room: string) {
   if (!rooms.has(room)) {
     rooms.set(room, new Set());
-    docs.set(room, new Y.Doc());
   }
 
   return {
     clients: rooms.get(room)!,
-    doc: docs.get(room)!,
   };
 }
-
 export function getRoom(room: string) {
   return rooms.get(room);
 }
