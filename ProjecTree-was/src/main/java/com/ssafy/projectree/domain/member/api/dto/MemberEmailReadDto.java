6645a54cf08e65f4aeb45b9c951af70cdab7e24c
이성훈 <qkghwkd153@naver.com>
2026-01-29 00:51:44 +0900

    feat: @Builder 추가 및 MemberNicknameReadDto 구현

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/member/api/dto/MemberEmailReadDto.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/member/api/dto/MemberEmailReadDto.java
@@ -2,11 +2,13 @@ package com.ssafy.projectree.domain.member.api.dto;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 
 public class MemberEmailReadDto {
 
     @Getter
+    @Builder
     @Schema(name = "MemberEmailReadDto.Response", description = "회원 이메일 조회 응답 DTO")
     public static class Response {
 
