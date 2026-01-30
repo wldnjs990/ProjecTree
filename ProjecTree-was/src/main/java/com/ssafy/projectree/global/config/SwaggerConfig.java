3a3452a432b4c72194cd97ccc5e4219fd212a8bd
진경석 <dev.jkjin112@gmail.com>
2026-01-30 16:50:02 +0900

    fix: swagger server url 수정 및 추가

--- ProjecTree-was/src/main/java/com/ssafy/projectree/global/config/SwaggerConfig.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/global/config/SwaggerConfig.java
@@ -2,9 +2,12 @@ package com.ssafy.projectree.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @OpenAPIDefinition(
         info = @Info(title = "ProjecTree WAS", description = "ProjecTree API 명세입니다.", version = "v1")
@@ -19,6 +22,10 @@ public class SwaggerConfig {
         return GroupedOpenApi.builder()
                 .group("ProjecTree API v1")
                 .pathsToMatch(paths)
+                .addOpenApiCustomizer(openApi -> openApi.servers(List.of(
+                        new Server().url("https://i14d107.p.ssafy.io/api").description("Production"),
+                        new Server().url("http://localhost:8080/api").description("Local")
+                )))
                 .build();
     }
 
