8000d8f11cc0bb8d8bff020e84fdc3124065cc07
성민제 <minj21@naver.com>
2026-02-02 10:30:40 +0900

    docs: 기술 스택 자동완성 문서 추가

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/tech/api/controller/TechStackController.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/tech/api/controller/TechStackController.java
@@ -4,6 +4,7 @@ import com.ssafy.projectree.domain.tech.api.dto.TechVocabularyDto;
 import com.ssafy.projectree.domain.tech.usecase.TechStackService;
 import com.ssafy.projectree.global.api.code.SuccessCode;
 import com.ssafy.projectree.global.api.response.CommonResponse;
+import com.ssafy.projectree.global.docs.TechStackDocsController;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,10 +14,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class TechStackController {
+public class TechStackController implements TechStackDocsController {
 
     private final TechStackService techStackService;
 
+    @Override
     @GetMapping("/tech-stacks/autocomplete")
     public CommonResponse<List<TechVocabularyDto>> autocomplete(@RequestParam String keyword) {
         return CommonResponse.success(SuccessCode.SUCCESS, techStackService.searchTechVocabulary(keyword));
