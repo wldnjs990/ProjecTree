85c7282717886f03c509161b692330756d290569
jiwon <wl990@naver.com>
2026-02-05 17:15:46 +0900

    refactor : 채팅 소캣 주소 임시 수정

--- ProjecTree-web/src/features/workspace-chat/services/chatSocket.ts
+++ ProjecTree-web/src/features/workspace-chat/services/chatSocket.ts
@@ -5,7 +5,8 @@ class ChatSocketService {
   private readonly serverUrl: string;
   constructor() {
     // 환경변수에서 WebSocket 서버 URL 가져오기
-    this.serverUrl = import.meta.env.VITE_WS_URL || 'ws://localhost:3000';
+    // this.serverUrl = import.meta.env.VITE_WS_URL || 'ws://localhost:3000';
+    this.serverUrl = 'https://i14d107.p.ssafy.io';
     // this.serverUrl = 'http://localhost:7092';
   }
 
