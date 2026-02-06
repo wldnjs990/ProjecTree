f0884eceb6900c35e98c60d24c242d81e9b01887
이성훈 <qkghwkd153@naver.com>
2026-02-06 10:05:26 +0900

    feat: CustomTechCreateDto에 API documentation 정보 세팅

--- ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/dto/CustomTechCreateDto.java
+++ ProjecTree-was/src/main/java/com/ssafy/projectree/domain/node/api/dto/CustomTechCreateDto.java
@@ -1,5 +1,6 @@
 package com.ssafy.projectree.domain.node.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,15 +12,24 @@ public class CustomTechCreateDto {
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "CustomTechCreateDto.Request", description = "커스텀 기술 스택 생성 요청")
     public static class Request {
+        @Schema(description = "워크스페이스 ID", example = "10")
         private Long workspaceId;
+
+        @Schema(description = "기술 단어(TechVocabulary) ID", example = "3")
         private Long techVocaId;
     }
 
     @Getter
     @Builder
+    @Schema(name = "CustomTechCreateDto.Response", description = "커스텀 기술 스택 생성 응답")
+
     public static class Response {
+
+        @Schema(description = "기술 단어(TechVocabulary) ID", example = "3")
         private Long techVocaId;
+        @Schema(description = "기술 이름", example = "java")
         private String techName;
     }
 }
