188453f40b9f67b41c5e4a018fee85214ccf8f2a
LeeSH <dlfyd05680@gmail.com>
2026-02-07 23:33:44 +0900

    refactor: 주석 제거

--- ProjecTree-CRDT/src/ws/room-broadcaster.ts
+++ ProjecTree-CRDT/src/ws/room-broadcaster.ts
@@ -2,8 +2,6 @@ import { WebSocket } from "ws";
 import { getOrCreateRoom, getAllRooms } from "./room-registry";
 
 export function sendToRoom(roomId: string, payload: unknown) {
-  // TODO
-  // 테스트 완료 후 주석 해제
   const { clients } = getOrCreateRoom(roomId);
   const message = JSON.stringify(payload);
   for (const client of clients) {
@@ -12,15 +10,6 @@ export function sendToRoom(roomId: string, payload: unknown) {
       console.log("Sent to client in room:", roomId);
     }
   }
-  // 테스트 완료 후 제거
-  // getAllRooms().forEach((room, id) => {
-  //   const message = JSON.stringify(payload);
-  //   for (const client of room.clients) {
-  //     if (client.readyState === WebSocket.OPEN) {
-  //       client.send(message);
-  //     }
-  //   }
-  // });
 }
 
 export function broadcastExceptSender(
