a9e1b4daa00269e02ff6082b0aa6b6d47de8343c
LeeSH <dlfyd05680@gmail.com>
2026-02-07 23:31:26 +0900

    refactor: 해당 룸의 유저에게만 전파하도록 수정

--- ProjecTree-CRDT/src/ws/room-broadcaster.ts
+++ ProjecTree-CRDT/src/ws/room-broadcaster.ts
@@ -4,28 +4,23 @@ import { getOrCreateRoom, getAllRooms } from "./room-registry";
 export function sendToRoom(roomId: string, payload: unknown) {
   // TODO
   // 테스트 완료 후 주석 해제
-  // const { clients } = getOrCreateRoom(roomId);
-  // const message = JSON.stringify(payload);
-
-  // for (const client of clients) {
-  //   if (client.readyState === WebSocket.OPEN) {
-  //     client.send(message);
-  //     console.log("Sent to client in room:", roomId);
-  //   }
-  // }
-
-  // 테스트 완료 후 제거
-  getAllRooms().forEach((room, id) => {
-    const message = JSON.stringify(payload);
-    for (const client of room.clients) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-        console.log(
-          `Sent to client in room:${roomId}, client: ${client} ${new Date().toISOString()}`,
-        );
-      }
+  const { clients } = getOrCreateRoom(roomId);
+  const message = JSON.stringify(payload);
+  for (const client of clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+      console.log("Sent to client in room:", roomId);
     }
-  });
+  }
+  // 테스트 완료 후 제거
+  // getAllRooms().forEach((room, id) => {
+  //   const message = JSON.stringify(payload);
+  //   for (const client of room.clients) {
+  //     if (client.readyState === WebSocket.OPEN) {
+  //       client.send(message);
+  //     }
+  //   }
+  // });
 }
 
 export function broadcastExceptSender(
