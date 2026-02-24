83794391275b9378f227b9b1f6f6122257a1a1b4
jiwon <wl990@naver.com>
2026-02-24 12:39:23 +0900

    feat : 도커 로컬 프로젝트 실행환경 구축(docker-compose) + env.example 생성

--- INFERENCE/app/core/config.py
+++ INFERENCE/app/core/config.py
@@ -23,8 +23,9 @@ class Settings(BaseSettings):
     LANGFUSE_SECRET_KEY: str
     LANGFUSE_PUBLIC_KEY: str
     LANGFUSE_BASE_URL: str
-    AI_API_KEY: str
+    OPENAI_API_KEY: str
     OPENAI_BASE_URL: str
+    ANTHROPIC_API_KEY: str
     ANTHROPIC_BASE_URL: str
 
     # CRDT Server 설정
